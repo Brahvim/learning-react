255d9bf801871c979f0495f9c3616b7ec2e7ae32
Brahvim <69293652+Brahvim@users.noreply.github.com>
2023-04-03 09:45:03 +0530

    Learnt to add a callback, I guess.

--- src/App.js
+++ src/App.js
@@ -2,23 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  //let logoSize = 1;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App" >
+      <header className="App-header" >
+        <div className="RotatingImage"
+          onPointerEnter={(e) => {
+            console.log("Mouse entered the image!");
+            // Resize the logo with some interpolation here, perhaps? :D?
+            // Would use CSS animations for this, right?!
+          }}
+
+          onPointerLeave={(e) => console.log("Mouse left the image!")}>
+          <img src={logo} className="App-logo" alt="logo" />
+        </div >
+        <p >Edit <code >src/App.js</code> and save to reload.</p>
+
+      </header >
+    </div >
   );
 }
 
